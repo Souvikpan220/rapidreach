216c3a99b3983c9e79e5f79ff631524ea99a2045
Souvikpan220 <117498210+Souvikpan220@users.noreply.github.com>
2026-01-21 18:29:23 +0530

    Update blacklist.js

--- api/blacklist.js
+++ api/blacklist.js
@@ -1,19 +1,1 @@
-// api/blacklist.js
 
-// Simple in-memory blacklist (can later be replaced with DB)
-const BLACKLISTED_IPS = new Set([
-  // example:
-  // "123.45.67.89"
-]);
-
-export function isBlacklisted(ip) {
-  return BLACKLISTED_IPS.has(ip);
-}
-
-export function addToBlacklist(ip) {
-  BLACKLISTED_IPS.add(ip);
-}
-
-export function removeFromBlacklist(ip) {
-  BLACKLISTED_IPS.delete(ip);
-}
